f3d819973a4993c7df8fee69babb546298080683
Mark <markestu@buffalo.edu>
2018-06-26 11:55:00 -0400

    Rotate 360 at each point, save images

--- src/go_to_specific_location.py
+++ src/go_to_specific_location.py
@@ -4,8 +4,12 @@ import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
-from geometry_msgs.msg import Pose, Point, Quaternion
-
+from geometry_msgs.msg import Pose, Point, Quaternion, Twist
+from sensor_msgs.msg import Image 
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 
+PI = 3.1415926535897
+bridge = CvBridge()
 class GoToPose():
     def __init__(self):
 
@@ -55,23 +59,56 @@ class GoToPose():
         rospy.loginfo("Stop")
         rospy.sleep(1)
 
+def image_callback(msg):
+	print("Reeived image")
+	try:
+		#convert image 
+		cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+	except CvBridgeError, e:
+		print(e)
+	else:
+		cv2.imwrite('camera_image{}.jpeg'.format(rospy.Time.now().to_sec()), cv2_img)	
+
 if __name__ == '__main__':
     try:
         rospy.init_node('nav_test', anonymous=False)
+        # Define the Twist compenent
+        rotation_publisher = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=10)
+        vel_msg = Twist()
+	angular_speed = 1
+        vel_msg.angular.z = 1
+	angle = 360*2*PI/360
         navigator = GoToPose()
-
+	# Subcribe to the images published by rgb and call the image_callback metho
+	rospy.Subscriber('/camera/rgb/image_raw', Image, image_callback)
         # Customize the following values so they are appropriate for your location
-        position1 = {'x': 11.6, 'y' : 4.28}
-        position2 = {'x': 0.0, 'y': 0.0}
+        position1 = {'x': 1.06, 'y' : -0.134}
+        position2 = {'x': 3.42, 'y': -3.81}
         quaternion = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
         success = navigator.goto(position1, quaternion)
         counter = 0
-        while success:
+        while success: 
             if success and counter == 0:
+                # Once succesful turn 360 degrees
+		current_angle = 0
+		t0 = rospy.Time.now().to_sec()
+		while  current_angle < angle:
+                	rotation_publisher.publish(vel_msg)
+			t1 = rospy.Time.now().to_sec()
+			current_angle = angular_speed*(t1 - t0)
+            	rospy.loginfo("Rotated")
+                # Go to next desired location
                 success = navigator.goto(position2, quaternion)
                 counter = 1
                 rospy.loginfo("Returning to position 2")
             elif success and counter == 1:
+		current_angle = 0
+		t0 = rospy.Time.now().to_sec()
+		while current_angle < angle:
+                	rotation_publisher.publish(vel_msg)
+			t1 = rospy.Time.now().to_sec()
+			current_angle = angular_speed*(t1 - t0)
+		rospy.loginfo("Rotated")
                 success = navigator.goto(position1, quaternion)
                 counter = 0
                 rospy.loginfo("Going to position1")
