3628503e3091d2284e1328f5374558fa3e7f4fe2
Mark <markestu@buffalo.edu>
2018-07-05 12:11:14 -0400

    Brushed up code

--- src/detecting2.py
+++ src/detecting2.py
@@ -38,6 +38,7 @@ PATH_TO_LABELS = os.path.join('/home/drones/models/research/object_detection/dat
     
 NUM_CLASSES = 90
 
+#Define the detection graph
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -57,68 +58,44 @@ category_index = label_map_util.create_category_index(categories)
 # Setting the GPU options to use fraction of gpu that has been set
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
+
+#Start the tf session 
 sess = tf.Session(graph = detection_graph, config = config)
 # Detection
-with detection_graph.as_default():
-	with tf.Session(graph=detection_graph,config=config) as sess2:
-		class detector:
+class detector:
 
-			def __init__(self):
-				self.image_pub = rospy.Publisher("debug_image",Image, queue_size=1)
-				self.object_pub = rospy.Publisher("objects", Detection2DArray, queue_size=1)
-				self.bridge = CvBridge()
-				self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_cb, queue_size=1, buff_size=2**24)
+	def __init__(self):
+		self.image_pub = rospy.Publisher("object_detection_image",Image, queue_size=1)
+		self.bridge = CvBridge()
+		self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_cb, queue_size=1, buff_size=2**24)
 
-			def image_cb(self, data):
-				objArray = Detection2DArray()
-				cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-				image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-				image_np = np.asarray(image)
-				image_np_expanded = np.expand_dims(image_np, axis = 0)
-				image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-				boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-				scores = detection_graph.get_tensor_by_name('detection_scores:0')
-				classes = detection_graph.get_tensor_by_name('detection_classes:0')
-				num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-				(boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections], feed_dict={image_tensor:image_np_expanded})
-				objects = vis_util.visualize_boxes_and_labels_on_image_array(
-					image,
-					np.squeeze(boxes),
-					np.squeeze(classes).astype(np.int32),
-					np.squeeze(scores),
-					category_index,
-					use_normalized_coordinates=True,
-					line_thickness=2)
+	def image_cb(self, data):
+		#Convert Image to cv2 bgr8
+		cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+		image_np = np.asarray(image)
+		image_np_expanded = np.expand_dims(image_np, axis = 0)
+		image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+		boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+		scores = detection_graph.get_tensor_by_name('detection_scores:0')
+		classes = detection_graph.get_tensor_by_name('detection_classes:0')
+		num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+		#Run model detection on Image
+		(boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections], feed_dict={image_tensor:image_np_expanded})
+		objects = vis_util.visualize_boxes_and_labels_on_image_array(
+			image,
+			np.squeeze(boxes),
+			np.squeeze(classes).astype(np.int32),
+			np.squeeze(scores),
+			category_index,
+			use_normalized_coordinates=True,
+			line_thickness=2)
 
-				# objArray.detections = []
-				# objArray.header = data.header
-				# object_count = 1
-
-				# for i in range(len(objects)):
-				# 	object_count+=1
-				# 	objArray.detections.append(self.object_predict(objects[i], data.header, image_np, cv_image))
-				img = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-				image_out = Image()
-				image_out = self.bridge.cv2_to_imgmsg(img, "bgr8")
-				image_out.header = data.header
-				self.image_pub.publish(image_out)
-
-			def object_predict(self, object_data, header, image_np, image):
-				image_height, image_width, channels = image.shape
-				obj = Detection2D()
-				obj_hypothesis = ObjectHypothesisWithPose()
-				object_id = object_data[0]
-				object_score = object_data[1]
-				dimensions = object_data[2]
-				obj.header = header
-				obj_hypothesis.id = object_id
-				obj_hypothesis.score = object_score
-				obj.results.append(obj_hypothesis)
-				obj.bbox.size_y = int((dimensions[2] - dimensions[0])*image_height)
-				obj.bbox.size_x = int((dimensions[2]-dimensions[1] )*image_width)
-				obj.bbox.center.x = int((dimensions[1] + dimensions [2])*image_height/2)
-				obj.bbox.center.y = int((dimensions[0] + dimensions[2])*image_width/2)
-				return obj
+		img = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+		image_out = Image()
+		image_out = self.bridge.cv2_to_imgmsg(img, "bgr8")
+		image_out.header = data.header
+		self.image_pub.publish(image_out)
 
 
 def main(args):
